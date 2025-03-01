86c65101d46959fbbe3eb4314141c8126476f9f9
JhorjanCH <93819202+JhorjanCH@users.noreply.github.com>
2025-02-28 23:45:26 -0400

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -120,7 +120,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                          with font-awesome or any other icon font library -->
 
 
-            @can('admin.index')
+            @can('usuarios.index')
 
             <li class="nav-item">
               <a href="{{url('/')}}" class="nav-link active">
