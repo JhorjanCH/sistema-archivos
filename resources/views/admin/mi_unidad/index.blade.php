4b7d5c1caddba3d69d6e9f0281daefef9c09951a
JhorjanCH <93819202+JhorjanCH@users.noreply.github.com>
2025-03-13 07:09:51 -0400

    Update index.blade.php

--- resources/views/admin/mi_unidad/index.blade.php
+++ resources/views/admin/mi_unidad/index.blade.php
@@ -185,7 +185,7 @@
     <div class="modal-dialog modal-dialog-centered">
       
       <div class="modal-content">
-        <div class="modal-header  border border-danger">
+        <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">Cambiar nombre</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
